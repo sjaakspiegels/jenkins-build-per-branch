07c83ed3e3dd0103eeb8ad8c5fcc26fb790a28c2
Sjaak Spiegels <Sjaak@voogd.com>
2017-07-09 14:39:29 +0200

    a

--- src/main/groovy/com/entagen/jenkins/TfsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/TfsApi.groovy
@@ -25,19 +25,7 @@ class TfsApi {
     Pattern branchNameFilter = null
 
     public List<String> getBranchNames() {
-        println " get branchnames ${branchNameFilter}"
- //       String command = "-u $tfsUser:$tfsToken ${tfsUrl}/_apis/tfvc/items?scopePath=${tfsCollection}"
 
- //       def response = [ 'bash', '-c', "curl ${command}" ].execute().text
-        //process.waitFor()
-        //println process.err.text
-        //println process.text
- //       println response
-//def http = new HTTPBuilder('$tfsUrl')
-//def html = http.get(path : '/_apis/tfvc/items', query : [scopePath:'$tfsCollection'])
-//        String res = doGetHttpRequest(command)
-//curl -u svctfsjenkins:agxqquxts22ty2dnh4wvunqvxmzckrd43w236z6p55qwlfvluiwa  http://10.100.10.161:8080/tfs/Voogd/_apis/tfvc/items?scopePath=$/Innovation%20Lab
- //       String command = "git ls-remote --heads ${gitUrl}"
        List<String> branchNames = []
 
        def list = getAllFolders(tfsCollection)
@@ -46,8 +34,13 @@ class TfsApi {
 
        def list2 = list.findAll { it.endsWith tfsProject}
 
-       println "filtered list ${tfsProject}"
-        list2.each { println it}
+       List<String> foundBranchNames = []
+
+        list2.each { 
+            foundBranchNames.add(it.replaceAll(tfsCollection , "").replaceAll("\\", "_"))
+        }
+            
+        foundBranchNames.each { println it}
    //    def paths = list.path
 
    //    paths.each { println it }
@@ -92,55 +85,5 @@ class TfsApi {
 
         return branchNames
     }
-
-    public Boolean passesFilter(String branchName) {
-        if (!branchName) return false
-        if (!branchNameFilter) return true
-        return branchName ==~ branchNameFilter
-    }
-
-    // assumes all commands are "safe", if we implement any destructive git commands, we'd want to separate those out for a dry-run
-    public void eachResultLine(String command, Closure closure) {
-        println "executing command: $command"
-        def process = command.execute()
-        def inputStream = process.getInputStream()
-        def gitOutput = ""
-
-        while(true) {
-          int readByte = inputStream.read()
-          if (readByte == -1) break // EOF
-          byte[] bytes = new byte[1]
-          bytes[0] = readByte
-          gitOutput = gitOutput.concat(new String(bytes))
-        }
-        process.waitFor()
-
-        if (process.exitValue() == 0) {
-            gitOutput.eachLine { String line ->
-               closure(line)
-          }
-        } else {
-            String errorText = process.errorStream.text?.trim()
-            println "error executing command: $command"
-            println errorText
-            throw new Exception("Error executing command: $command -> $errorText")
-        }
-    }
-
-//    HttpResponse doGetHttpRequest(String requestUrl) {
-//        println "RequestUrl: $requestUrl"
-
-//        URL url = new URL(requestUrl)
-//        HttpURLConnection connection = url.openConnection()
-
-//        connection.setRequestMethod("GET")
-
-//        HttpResponse resp = new HttpResponse(connection)
-
-//        println "Response: $resp.message"
-//        println "Response-body: $resp.body"
-        
-//        return resp
-//    }
 
 }
