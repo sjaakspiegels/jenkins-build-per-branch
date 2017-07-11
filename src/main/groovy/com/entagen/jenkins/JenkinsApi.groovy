76b7ea1618886c2ac0e0c2b11540e00ad4ed6f9e
Sjaak Spiegels <Sjaak@voogd.com>
2017-07-11 11:58:17 +0200

    a

--- src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
@@ -91,6 +91,9 @@ println config
             }
         }
 
+        config = config.replaceFirst("<projectPath>[^<]*</projectPath>", missingJob.path)
+
+
         // this is in case there are other down-stream jobs that this job calls, we want to be sure we're replacing their names as well
         templateJobs.each {
             config = config.replaceAll(it.jobName, it.jobNameForBranch(missingJob.branchName))
