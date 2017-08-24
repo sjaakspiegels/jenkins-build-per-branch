53c9b7ec0c899a24f7ce8d1cd9b0730fd524d652
Sjaak Spiegels <Sjaak@voogd.com>
2017-08-24 08:41:41 +0200

    dasd

--- src/main/groovy/com/entagen/jenkins/TfsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/TfsApi.groovy
@@ -122,7 +122,7 @@ class TfsApi {
                             "   \"scope\":1, " +
                             "   \"consumerInputs\":{ " +
                             "       \"serverBaseUrl\":\"${jenkinsUrl}\", " +
-                            "       \"username\":\"${tfsUser}\", " +
+                            "       \"username\":\"${jenkinsUser}\", " +
                             "       \"password\":\"${jenkinsPassword}\", " +
                             "       \"buildName\":\"${job.jobName}\", " +
                             "       \"useTfsPlugin\":\"built-in\"}, " +
